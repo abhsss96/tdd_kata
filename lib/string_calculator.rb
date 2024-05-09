4ae70b26d39f2b784ebbef27434c027012f277e0
Abhishek Sharma <abhsss96@gmail.com>
2024-05-09 15:06:53 +0530

    Improved coverage.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,4 +1,6 @@
 class StringCalculator
+  REGEX = %r{^//(\D+)(\\n|\n)(.+)}
+
   def initialize(numbers_string)
     @numbers_string = numbers_string
     @delimiter = /[,\\n]/
@@ -6,14 +8,17 @@ class StringCalculator
 
   def calculate
     return 0 if @numbers_string.empty?
-
     raise 'Invalid input' if @numbers_string.match?(/,\\n|\\n,/)
 
     if @numbers_string.start_with?('//')
-      match = @numbers_string.match(%r{^//(.+)\n})
+      # require 'pry'
+      # binding.pry
+      match = @numbers_string.match(REGEX)
+      raise 'invalid input' unless match
+
       @delimiter = match[1]
 
-      @numbers_string = @numbers_string[(match[0].length)..]
+      @numbers_string = match[3]
     end
 
     numbers = @numbers_string.split(@delimiter).map(&:to_i)
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -3,69 +3,137 @@ require 'string_calculator'
 RSpec.describe StringCalculator do
   let(:subject) { described_class.new(input) }
 
-  context 'with an empty string' do
-    let(:input) { '' }
+  context 'with default delimiter' do
+    context 'with an empty string' do
+      let(:input) { '' }
 
-    it 'returns 0' do
-      expect(subject.calculate).to eq(0)
+      it 'returns 0' do
+        expect(subject.calculate).to eq(0)
+      end
+    end
+
+    context 'with a single number' do
+      let(:input) { '1' }
+      it 'returns that number' do
+        expect(subject.calculate).to eq(1)
+      end
+    end
+
+    context 'with a string of numbers' do
+      let(:input) { '1,2,3' }
+
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(6)
+      end
+    end
+
+    context 'with n numbers' do
+      let(:input) { (1...100).to_a.join(',') }
+
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(4950)
+      end
+    end
+
+    context 'with invalid input like ,\n' do
+      let(:input) { '1,\n' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('Invalid input')
+      end
+    end
+
+    context 'with invalid input like \n,' do
+      let(:input) { '1,\n' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('Invalid input')
+      end
+    end
+
+    context 'with newline character and comma separated numbers' do
+      let(:input) { '1\n2,3' }
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(6)
+      end
+    end
+
+    context 'with delimiter specified' do
+      let(:input) { "//;\n1;2" }
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(3)
+      end
+    end
+
+    context 'with a negative number' do
+      let(:input) { '1,2,3,-4' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('negative numbers not allowed -4')
+      end
+    end
+    context 'with multiple negative numbers' do
+      let(:input) { '1,2,3,-4,-5' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('negative numbers not allowed -4,-5')
+      end
     end
   end
 
-  context 'with a single number' do
-    let(:input) { '1' }
-    it 'returns that number' do
-      expect(subject.calculate).to eq(1)
+  context 'with custom delimiter' do
+    context 'with a single number' do
+      let(:input) { '//;\n1' }
+      it 'returns that number' do
+        expect(subject.calculate).to eq(1)
+      end
     end
-  end
 
-  context 'with a string of numbers' do
-    let(:input) { '1,2,3' }
+    context 'with a string of numbers' do
+      let(:input) { '//;\n1;2;3' }
 
-    it 'returns the sum of the numbers' do
-      expect(subject.calculate).to eq(6)
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(6)
+      end
     end
-  end
 
-  context 'with n numbers' do
-    let(:input) { (1...100).to_a.join(',') }
+    context 'with n numbers' do
+      let(:input) { "//;\n#{(1...100).to_a.join(';')}" }
 
-    it 'returns the sum of the numbers' do
-      expect(subject.calculate).to eq(4950)
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(4950)
+      end
     end
-  end
 
-  context 'with invalid input like ,\n' do
-    let(:input) { '1,\n' }
-    it 'raises an error' do
-      expect { subject.calculate }.to raise_error('Invalid input')
+    context 'with invalid input like ,\n' do
+      let(:input) { '//;\n1,\n' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('Invalid input')
+      end
     end
-  end
 
-  context 'with invalid input like \n,' do
-    let(:input) { '1,\n' }
-    it 'raises an error' do
-      expect { subject.calculate }.to raise_error('Invalid input')
+    context 'with invalid input like \n,' do
+      let(:input) { '//;\n1,\n' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('Invalid input')
+      end
     end
-  end
 
-  context 'with newline character and comma separated numbers' do
-    let(:input) { '1\n2,3' }
-    it 'returns the sum of the numbers' do
-      expect(subject.calculate).to eq(6)
+    context 'with delimiter specified' do
+      let(:input) { "//;\n1;2" }
+      it 'returns the sum of the numbers' do
+        expect(subject.calculate).to eq(3)
+      end
     end
-  end
 
-  context 'with delimiter specified' do
-    let(:input) { "//;\n1;2" }
-    it 'returns the sum of the numbers' do
-      expect(subject.calculate).to eq(3)
+    context 'with a negative number' do
+      let(:input) { '//;\n1;2;3;-4' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('negative numbers not allowed -4')
+      end
     end
-  end
 
-  context 'with a negative number' do
-    let(:input) { '1,2,3,-4' }
-    it 'raises an error' do
-      expect { subject.calculate }.to raise_error('negative numbers not allowed -4')
+    context 'with multiple negative numbers' do
+      let(:input) { '//;\n1;2;3;-4;-5' }
+      it 'raises an error' do
+        expect { subject.calculate }.to raise_error('negative numbers not allowed -4,-5')
+      end
     end
   end
 end
